60556731ef6a3a0f1f51def2698aa09dc6eda9ce
rgoldschmid <116354147+rgoldschmid@users.noreply.github.com>
2024-03-06 16:20:45 +0100

    added input and a function

--- 01_lecture/reverse.py
+++ 01_lecture/reverse.py
@@ -3,9 +3,20 @@ Analyse the code below and try to find out what it does!
 
 Even though you don't know a lot of the keywords yet, you might be able to understand what's going on by reading the
 code line by line. To start out, you can try to "translate" the code in natural language.
+
+You can run the code by clicking the green Play-Button on the top right of your screen or by pressing Shift + F10
 """
 
-groceries = [
+
+def apply_printing_format_to_number(number):
+    return str(round(number, 2))
+
+
+name = input("What is your name?\n")
+budget = float(input("What's your current budget?\n"))
+print("Hello, " + name + "! Your budget is " + apply_printing_format_to_number(budget) + "€. Let's go shopping!")
+
+groceries_to_buy = [
     ["Apple", 5, 0.45],
     ["Banana", 8, 0.80],
     ["Onion", 4, 0.70],
@@ -18,32 +29,31 @@ groceries = [
 
 sum_total = 0
 items_total = 0
-cheap_items_count = 0
-normal_items_count = 0
-expensive_items_count = 0
 
-for item in groceries:
-    name, qty, price = item
+print("The following items are on your grocery-list:")
+for item in groceries_to_buy:
+    item_name, qty, price = item
+    print(str(qty) + "x " + item_name + " á " + apply_printing_format_to_number(price) + "€")
 
     items_total = items_total + qty
-
     sum_total = sum_total + (qty * price)
 
-    if price > 0 and price <= 4.99:
-        cheap_items_count = cheap_items_count + qty
-    elif 4.99 < price <= 14.99:
-        normal_items_count = normal_items_count + qty
-    else:
-        expensive_items_count = expensive_items_count + qty
-
+budget_difference = budget - sum_total
 average_price = sum_total / items_total
-average_price = round(average_price, 2)
 
-total_sum_as_string = str(sum_total)
-
-print("Total sum to pay: " + total_sum_as_string + " €")
-print("The average price is " + str(average_price) + " € per item.")
-print("Amount of cheap items bought:", cheap_items_count)
-print("Amount of normal items bought", normal_items_count)
-print("Amount of expensive items bought", expensive_items_count)
+print("---")
 print("Total items bought:", items_total)
+print("Total sum to pay: " + apply_printing_format_to_number(sum_total) + "€")
+print("The average price is " + apply_printing_format_to_number(average_price) + "€ per item.")
+print("---")
+
+if budget_difference < 0:
+    print("Sorry, you have made " + apply_printing_format_to_number(budget_difference * (-1)) + "€ in debt!")
+elif budget_difference > 0:
+    print(
+        "Congratulation, after buying your groceries, you have " + apply_printing_format_to_number(budget_difference) +
+        "€ left!")
+else:
+    print("You have bought all your groceries but no more money!")
+
+print("Goodbye " + name + "!")
