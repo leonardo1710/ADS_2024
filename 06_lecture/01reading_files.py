19971b824a4becd533982e8bb568040514a0f9a2
leonardo1710 <leon.freudenthaler@fh-campuswien.ac.at>
2024-05-07 14:34:25 +0200

    lecture 6 in progress

---
+++ 06_lecture/01reading_files.py
@@ -0,0 +1,33 @@
+"""
+A simple way to include files in a Python program is to use the with statement.
+The header line opens the file, and the block where the file can be accessed follows.
+After the block the file is automatically closed, and can no longer be accessed.
+
+So, the following code opens the file, reads the contents, prints them out, and then closes the file:
+"""
+with open("example.txt") as new_file:
+    contents = new_file.read()
+    print(contents)
+
+"""
+Going through the contents of a file
+
+The read method is useful for printing out the contents of the entire file, but more often we will want to go through the file line by line.
+
+Text files can be thought of as lists of strings, each string representing a single line in the file. We can go through the list with a for loop.
+
+The following example reads our example file using a for loop, removes line breaks from the end of each line, counts the number of lines, and prints each line with its line number. 
+It also keeps track of the length of the lines:
+"""
+with open("example.txt") as new_file:
+    count = 0
+    total_length = 0
+
+    for line in new_file:
+        line = line.replace("\n", "")
+        count += 1
+        print("Line", count, line)
+        length = len(line)
+        total_length += length
+
+print("Total length of lines:", total_length)
---
+++ 06_lecture/exercises/01reading_files.py
@@ -0,0 +1,8 @@
+"""
+The file numbers.txt contains integer numbers, one number per line.
+
+Write a function named largest, which reads the file and returns the largest number in the file.
+
+Notice that the function does not take any arguments. The file you are working with is always named numbers.txt.
+"""
+
